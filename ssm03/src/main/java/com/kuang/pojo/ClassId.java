0ddf019265b91e80dc9e1775b998426058119d19
huangsiting <1736556310@qq.com>
2020-12-27 10:19:57 +0800

    修改classId

---
+++ ssm03/src/main/java/com/kuang/pojo/ClassId.java
@@ -0,0 +1,15 @@
+package com.kuang.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ClassId {
+    private int classId;
+    private String className;
+}
